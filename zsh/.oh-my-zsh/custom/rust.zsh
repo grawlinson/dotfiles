dc4eb36b57bd2588dbc0355deb91cd5ac505bdb5
George Rawlinson <george@rawlinson.net.nz>
2017-01-21 18:16:49 +1300

    Update zsh files

--- zsh/.oh-my-zsh/custom/rust.zsh
+++ zsh/.oh-my-zsh/custom/rust.zsh
@@ -1,7 +1,3 @@
 export RUST_SRC_PATH=~/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 
-alias docker="sudo docker"
-
 export PATH="$PATH:$HOME/.cargo/bin"
-
-export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
