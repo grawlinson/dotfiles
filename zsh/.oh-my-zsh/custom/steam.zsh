95764aef08bf34e7725195f18ae26d66fb01ff4a
George Rawlinson <george@rawlinson.net.nz>
2018-07-23 12:39:59 +1200

    revert(zsh): remove obsolete scripts
    - alert
    - steam
    - vita

--- zsh/.oh-my-zsh/custom/steam.zsh
+++
@@ -1,2 +0,0 @@
-# Force Steam to close to tray instead of remaining open
-export STEAM_FRAME_FORCE_CLOSE=1
--- zsh/.oh-my-zsh/custom/vita.zsh
+++
@@ -1,3 +0,0 @@
-# Define VITASDK & export to $PATH
-export VITASDK=/usr/local/vitasdk
-export PATH="$VITASDK/bin:$PATH"
