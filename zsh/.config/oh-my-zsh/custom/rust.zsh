8ccaaf588fc46c8eedc4157aa9f3295a142911f3
George Rawlinson <george@rawlinson.net.nz>
2018-08-06 19:12:40 +1200

    revert(zsh): remove $RUST_SRC_PATH variable

--- zsh/.config/oh-my-zsh/custom/rust.zsh
+++ zsh/.config/oh-my-zsh/custom/rust.zsh
@@ -1,5 +1,2 @@
-# Export rust-src path for Linux
-export RUST_SRC_PATH=~/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
-
 # Add cargo binary directory to $PATH
 export PATH="$PATH:$HOME/.cargo/bin"
