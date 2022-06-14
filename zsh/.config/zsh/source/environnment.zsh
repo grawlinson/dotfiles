9905ded582314ad6250dfcedf856909f8885c5c0
George Rawlinson <george@rawlinson.net.nz>
2022-06-14 20:13:19 +1200

    feat(zsh): update env vars (EDITOR/COLORTERM)

--- zsh/.config/zsh/source/environnment.zsh
+++ zsh/.config/zsh/source/environnment.zsh
@@ -2,8 +2,18 @@
 # environment variables
 #
 
-export EDITOR="nvim"
+# editor
+if command -v nvim &> /dev/null; then
+  export EDITOR='nvim'
+elif command -v vim &> /dev/null; then
+  export EDITOR='vim'
+fi
 
 # locale
 export LANG='en_NZ.UTF-8'
 export LC_ALL='en_NZ.UTF-8'
+
+# more terminal futzing. yay.
+export COLORTERM='24bit'
+
+# vim: ft=zsh expandtab tabstop=2 shiftwidth=2
